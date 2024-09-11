ba862d8e17b7e41e1058ef7bb8648c6a4cab72e4
JGE0211 <ke8650@gmail.com>
2024-09-11 14:03:44 +0900

    1/2주차

---
+++ 007/007.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	float weight, height;
+
+	printf("몸무게(kg) :");
+	scanf_s("%f", &weight);
+	printf("키(cm) :");
+	scanf_s("%f", &height);
+
+	height /= 100;   // m단위로 변환
+	
+	float bmi = weight / (height * height);
+	printf("bmi : %.1f\n", bmi);
+}
