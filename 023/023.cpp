186480faaf8ffde2d5a8ae839ec65019ea8aa29f
JGE0211 <ke8650@gmail.com>
2024-10-06 17:00:04 +0900

    Add files via upload

---
+++ 023/023.cpp
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	////2중반복문
+	////외워주세요*******
+	for (int y = 1; y <= 9; y++){ 
+		for (int x = 2; x <= 5; x++) {
+			printf("%d x %d = %d\t", x, y, x * y);
+		}
+		printf("\n");
+	}
+
+	printf("\n");
+
+	for (int y = 1; y <= 9; y++) {
+		for (int x = 6; x <= 9; x++) {
+			printf("%d x %d = %d\t", x, y, x * y);
+		}
+		printf("\n");
+	}
+	
+}
---
+++ 024/024.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int n;
+	printf("자연수 입력: ");
+	scanf_s("%d", &n);
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= 2 * i - 1; j++) {  //////*******2*i-1 이런 규칙들을 잘 찾기******
+			printf("%d ", j);
+		}
+		printf("\n");
+	}
+}
