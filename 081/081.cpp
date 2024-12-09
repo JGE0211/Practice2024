80ed20835e76037897eca4953fcc5d8dc3b75b41
JGE0211 <ke8650@gmail.com>
2024-12-10 03:57:13 +0900

    Add files via upload

---
+++ 081/081.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int square(int n) { return n * n; }
+int cube(int n) { return n * n * n; }
+
+int GetMax(int a[], int n, int(*f)(int)) {
+	int max = f(a[0]);
+	for (int i = 1; i < n; i++)
+		if (max < f(a[i]))
+			max = f(a[i]);
+	return max;
+}
+
+int main()
+{
+	int a[6] = { 3, 1, -4, 5, 9, -2 };
+
+	cout << GetMax(a, 6, square) << endl;
+	cout << GetMax(a, 6, cube) << endl;
+}
---
+++ 083/083.cpp
@@ -0,0 +1,25 @@
+﻿//STL: Standard Template Library
+#include <iostream>
+#include <vector>
+//#include <string>  /// 스트링 쓰는 지 확인
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	vector<string> v;  //template 라이브러리
+	string s;
+
+	cout << "문자열 10개 입력 :";
+	for (int i = 0; i < 10; i++) {
+		cin >> s;
+		v.push_back(s);
+	}
+
+	sort(v.begin(), v.end());
+
+	for (int i = 0; i < 10; i++) {
+		cout << v[i] << " ";
+	}
+	cout << endl;
+}
